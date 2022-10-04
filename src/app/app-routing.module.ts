cb0a8cce805c360c6da4d3dec833dc3142117021
Juan Carlos Quispe <jcquispe006@gmail.com>
2022-10-04 17:39:52 -0400

    dist folder added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,10 +29,6 @@ const routes: Routes = [
     component: VentasComponent
   },
   {
-    path: 'kardex',
-    component: InventarioComponent
-  },
-  {
     path: '**', pathMatch: 'full', redirectTo: 'login'
   }
 ];
